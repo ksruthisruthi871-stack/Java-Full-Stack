505b37599febb2824870b229b5e29240192f8c67
ksruthisruthi871-stack <ksruthisruthi871@gmail.com>
2026-06-12 12:12:09 +0530

    manipulation

---
+++ manipulation.js
@@ -0,0 +1,12 @@
+/*let pen=document.getElementById("wish");
+console.log(pen);
+let mouse=document.getElementsByClassName("wish");
+console.log(mouse);
+let monitor=document.getElementsByTagName("div");
+console.log(monitor);
+let wish=document.querySelector("#content");
+console.log(wish);
+let water=document.querySelectorAll("p");
+console.log(water);*/
+let value=documengt.getElementById("h3");
+value.innerText="happy birthday komala";
